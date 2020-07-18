9c203eb7e205244314f24c947060dc0d49d7b85e
Alexander Krause <akr@informatik.uni-kiel.de>
2020-07-18 11:02:48 +0200

    cleanup of setup

--- kind/kind.sh
+++ kind/kind.sh
@@ -1,26 +1,31 @@
 #!/bin/bash
 
 setup() {
+
+  echo creating explorviz kind cluster
+  kind create cluster --wait 3m --name explorviz-dev --config=kind-config.yaml
+
+  echo
+  echo setting kubectl context to that cluster
+  kind export kubeconfig --name explorviz-dev
+
+  echo
   echo adding helm repo  
   helm repo add bitnami https://charts.bitnami.com/bitnami
 
   echo
-  echo creating kubernetes namespace
-  kubectl create namespace explorviz-dev
-
-  echo
   echo installing kafka
-  helm install -n explorviz-dev -f kafka/values.yml kafka bitnami/kafka
+  helm install -f kafka/values.yml kafka bitnami/kafka
   sleep 1
   
   echo
   echo installing the schema registry
-  kubectl create --namespace=explorviz-dev -f registry/manifest.yml
+  kubectl create -f registry/manifest.yml
   sleep 1
   
   echo
   echo installing apache cassandra
-  helm install -n explorviz-dev -f cassandra/values.yml cassandra bitnami/cassandra
+  helm install -f cassandra/values.yml cassandra bitnami/cassandra
 
   sleep 1
 
@@ -32,29 +37,20 @@ setup() {
   echo
   echo kafka is ready
   echo creating topics
-  kubectl exec --namespace=explorviz-dev kafka-0 -- kafka-topics.sh --create --zookeeper kafka-zookeeper:2181 --replication-factor 1 --partitions 1 --topic cluster-dump-spans
-  kubectl exec --namespace=explorviz-dev kafka-0 -- kafka-topics.sh --create --zookeeper kafka-zookeeper:2181 --replication-factor 1 --partitions 1 --topic explorviz-spans
-  kubectl exec --namespace=explorviz-dev kafka-0 -- kafka-topics.sh --create --zookeeper kafka-zookeeper:2181 --replication-factor 1 --partitions 1 --topic explorviz-traces
-  kubectl exec --namespace=explorviz-dev kafka-0 -- kafka-topics.sh --create --zookeeper kafka-zookeeper:2181 --replication-factor 1 --partitions 1 --topic explorviz-records
+  kubectl exec kafka-0 -- kafka-topics.sh --create --zookeeper kafka-zookeeper:2181 --replication-factor 1 --partitions 1 --topic cluster-dump-spans
+  kubectl exec kafka-0 -- kafka-topics.sh --create --zookeeper kafka-zookeeper:2181 --replication-factor 1 --partitions 1 --topic explorviz-spans
+  kubectl exec kafka-0 -- kafka-topics.sh --create --zookeeper kafka-zookeeper:2181 --replication-factor 1 --partitions 1 --topic explorviz-traces
+  kubectl exec kafka-0 -- kafka-topics.sh --create --zookeeper kafka-zookeeper:2181 --replication-factor 1 --partitions 1 --topic explorviz-records
 
   # Install the service after Kafka is ready
   echo
   echo installing opencensus service
-  kubectl create --namespace=explorviz-dev -f oc-collector/manifest.yml
+  kubectl create -f oc-collector/manifest.yml
 }
 
 shutdown() {
-  echo removing deployments, pods, and services
-  kubectl delete --namespace=explorviz-dev deploy,po,svc,statefulsets --all
-
-  echo
-  echo removing statefulsets
-  kubectl delete --namespace=explorviz-dev statefulsets --all
-
-  echo
-  echo removing persistentvolumes
-  kubectl delete --namespace=explorviz-dev persistentvolumeclaims --all
-  kubectl delete --namespace=explorviz-dev persistentvolume --all
+  kind delete cluster --name explorviz-dev
+  kubectl cluster-info --context default
 }
 
 if [ "$(type -t $1)" == 'function' ]; then
