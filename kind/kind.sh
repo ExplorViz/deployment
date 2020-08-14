04e19160d671477d00549245e80acdc9c0245bc6
Kevin Lotz <lotzk@outlook.com>
2020-08-14 09:19:12 +0200

    Move topic creation to other script

--- kind/kind.sh
+++ kind/kind.sh
@@ -41,12 +41,7 @@ setup() {
   echo
   echo kafka is ready
   echo creating topics
-  kubectl exec kafka-0 -- kafka-topics.sh --create --zookeeper kafka-zookeeper:2181 --replication-factor 1 --partitions 1 --topic cluster-dump-spans
-  kubectl exec kafka-0 -- kafka-topics.sh --create --zookeeper kafka-zookeeper:2181 --replication-factor 1 --partitions 1 --topic explorviz-spans
-  kubectl exec kafka-0 -- kafka-topics.sh --create --zookeeper kafka-zookeeper:2181 --replication-factor 1 --partitions 1 --topic explorviz-traces
-  kubectl exec kafka-0 -- kafka-topics.sh --create --zookeeper kafka-zookeeper:2181 --replication-factor 1 --partitions 1 --topic explorviz-records
-  kubectl exec kafka-0 -- kafka-topics.sh --create --zookeeper kafka-zookeeper:2181 --replication-factor 1 --partitions 1 --topic explorviz-spans-structure
-
+  ./create_topic.sh
   # Install the service after Kafka is ready
   echo
   echo installing opencensus service
