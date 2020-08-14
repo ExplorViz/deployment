bc022f0561b5c77ac166989edc3082b2eeaa5a45
Kevin Lotz <lotzk@outlook.com>
2020-08-14 10:05:08 +0200

    Add missing topic for traces

--- kind/create_topics.sh
+++ kind/create_topics.sh
@@ -5,3 +5,4 @@ kubectl exec kafka-0 -- kafka-topics.sh --create --zookeeper kafka-zookeeper:218
 kubectl exec kafka-0 -- kafka-topics.sh --create --zookeeper kafka-zookeeper:2181 --replication-factor 1 --partitions 1 --topic explorviz-traces
 kubectl exec kafka-0 -- kafka-topics.sh --create --zookeeper kafka-zookeeper:2181 --replication-factor 1 --partitions 1 --topic explorviz-records
 kubectl exec kafka-0 -- kafka-topics.sh --create --zookeeper kafka-zookeeper:2181 --replication-factor 1 --partitions 1 --topic explorviz-spans-structure
+kubectl exec kafka-0 -- kafka-topics.sh --create --zookeeper kafka-zookeeper:2181 --replication-factor 1 --partitions 1 --topic explorviz-spans-dynamic
