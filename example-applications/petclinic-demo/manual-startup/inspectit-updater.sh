5b7320ee55010ef94d0643c43aa854069f8a77c2
Alexander Krause <akr@informatik.uni-kiel.de>
2021-05-26 16:33:38 +0200

    updated petclinic to fully working example and Docker-only

--- example-applications/petclinic-demo/manual-startup/inspectit-updater.sh
+++
@@ -1,5 +0,0 @@
-sed -i "s#landscape-token#$LANDSCAPE_TOKEN#g" inspectit/inspectit.yml
-
-sed -i "s#landscape-secret#$LANDSCAPE_SECRET#g" inspectit/inspectit.yml
-
-sed -i "s#localhost:55678#$OC_COLLECTOR_ADDRESS#g" inspectit/inspectit.yml
