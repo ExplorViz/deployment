fbbed5209d504971c72e5316e741e88ce474f0a3
Alexander Krause <akr@informatik.uni-kiel.de>
2023-01-17 12:21:36 +0100

    prepare otel logs

--- docker/generic/kafka/init_kafka_topics.sh
+++ docker/generic/kafka/init_kafka_topics.sh
@@ -4,8 +4,9 @@ echo -e 'Creating kafka topics'
 
 kafka-topics --bootstrap-server kafka:${KAFKA_INT_PORT} --create --if-not-exists --topic otlp_spans --replication-factor 1 --partitions 20
 
-kafka-topics --bootstrap-server kafka:${KAFKA_INT_PORT} --create --if-not-exists --topic explorviz-spans --replication-factor 1 --partitions 20
+kafka-topics --bootstrap-server kafka:${KAFKA_INT_PORT} --create --if-not-exists --topic otlp_logs --replication-factor 1 --partitions 20
 
+kafka-topics --bootstrap-server kafka:${KAFKA_INT_PORT} --create --if-not-exists --topic explorviz-spans --replication-factor 1 --partitions 20
 
 kafka-topics --bootstrap-server kafka:${KAFKA_INT_PORT} --create --if-not-exists --topic token-events --replication-factor 1 --partitions 20
 
