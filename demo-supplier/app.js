f66a2898477750384522a7ba9a2a0095548848fa
Malte Hansen <malte.hanseat@gmail.com>
2025-05-26 14:05:56 +0200

    Use snapshot of user service for demo supplier

--- demo-supplier/app.js
+++ demo-supplier/app.js
@@ -4,14 +4,6 @@ const cors = require("cors");
 const compression = require("compression");
 const fs = require("fs");
 const path = require("path");
-const {
-  removeRandomTraces,
-  recursivelyRandomizeAllHashCodesOfPackages,
-  copyPackageAndTraces,
-  createRandomHex,
-  calculateTenSecondLaterNeighbourTimestamp,
-} = require("./utils.js");
-
 const spanApp = createExpressApplication(8083);
 const userApp = createExpressApplication(8084);
 const evolutionApp = createExpressApplication(8085);
@@ -65,123 +57,6 @@ async function iterateOverDemoData(directoryPath) {
     });
   });
 }
-
-// Expanding PetClinic
-createLandscapeSample({
-  folder: "PetClinic Sample",
-  token: "19844195-7235-4254-a17b-0f7fb49adb0a",
-  alias: "Petclinic Sample (Random traces and increasing, unrelated timestamps (with random gaps))",
-  traceModifier: removeRandomTraces,
-  timestampModifier: (latestTimestandEpochMilli) => {
-    let nextTimestampMilli = calculateTenSecondLaterNeighbourTimestamp(parseInt(latestTimestandEpochMilli));
-    let randomSpanCount = parseInt(Math.random() * (150 - 50) + 50);
-
-    if (Math.random() > 0.75) {
-      nextTimestampMilli += 10000;
-    }
-
-    return {
-      epochMilli: nextTimestampMilli,
-      spanCount: randomSpanCount,
-    };
-  },
-});
-
-// BEGIN BIG SL Sample
-createLandscapeSample({
-  folder: "PetClinic Sample",
-  token: "1d8c9223-b790-4873-9b5d-fdf68cdc082f",
-  alias: "Large Landscape Sample",
-  initializer: (structure, traces) => {
-    const originalTraces = structuredClone(traces);
-
-    const node = structure.nodes[0];
-    const app = node.applications[0];
-    const package = app.packages[0];
-
-    app.name = "large-demo-landscape";
-
-    app.packages.unshift({
-      name: "changing",
-      subPackages: [],
-      classes: Array.from({ length: 12 }, () => ({
-        name: "C" + createRandomHex(6),
-        methods: [],
-      })),
-    });
-
-    for (let i = 0; i < 15; i++) {
-      const { packageCopy, newTraces } = copyPackageAndTraces(package, originalTraces);
-
-      app.packages.push({
-        name: `petclinic${i}`,
-        subPackages: [packageCopy],
-        classes: [],
-      });
-
-      traces.push(...newTraces);
-    }
-  },
-  structureModifier: (structure) => {
-    const changingPackage = structure.nodes[0].applications[0].packages[0];
-    changingPackage.classes.forEach((c) => (c.name = "C" + createRandomHex(6)));
-    return structure;
-  },
-});
-// END BIG SL Sample
-
-{
-  // BEGIN Increasing SL Sample
-  let previousStructure = null;
-
-  createLandscapeSample({
-    folder: "Distributed PetClinic Sample",
-    token: "12444195-6144-4254-a17b-0f7fb49adb0a",
-    alias: "Expanding Sample (Expanding structure and increasing, unrelated timestamps)",
-    structureModifier: (structureData) => {
-      if (!previousStructure) {
-        previousStructure = structuredClone(structureData);
-        return previousStructure;
-      }
-
-      const node = structureData.nodes[0];
-      const app = node.applications[0];
-      const package = app.packages[0];
-
-      const newStructure = addTopLevelPackageToFirstApplication(package, previousStructure);
-      previousStructure = newStructure;
-
-      return previousStructure;
-    },
-    timestampModifier: (latestTimestandEpochMilli) => {
-      const nextTimestampMilli = calculateTenSecondLaterNeighbourTimestamp(parseInt(latestTimestandEpochMilli));
-      const randomSpanCount = parseInt(Math.random() * (150 - 50) + 50);
-
-      return {
-        epochMilli: nextTimestampMilli,
-        spanCount: randomSpanCount,
-      };
-    },
-  });
-
-  function addTopLevelPackageToFirstApplication(topLevelPackage, structureRecord) {
-    const deepCopyPackage = structuredClone(topLevelPackage);
-    recursivelyRandomizeAllHashCodesOfPackages(deepCopyPackage);
-
-    const node = structureRecord.nodes[0];
-    const app = node.applications[0];
-
-    const newTopLevelPackage = {
-      name: `copy${app.packages.length - 1}`,
-      subPackages: [deepCopyPackage],
-      classes: [],
-    };
-
-    app.packages.push(newTopLevelPackage);
-
-    return structureRecord;
-  }
-} // END Increasing SL Sample
 
 /**
  * Creates and configures a express application instance.
