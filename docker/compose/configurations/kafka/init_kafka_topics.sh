6378808daaec66b8cffd57c1d79536f6ba2ace6d
Moritz Bugla <stu237520@mail.uni-kiel.de>
2026-06-20 17:51:09 +0200

    Remove unused tokens.events.table topic

--- docker/compose/configurations/kafka/init_kafka_topics.sh
+++ docker/compose/configurations/kafka/init_kafka_topics.sh
@@ -6,10 +6,7 @@ kafka-topics --bootstrap-server kafka:${KAFKA_INT_PORT} --create --if-not-exists
 
 kafka-topics --bootstrap-server kafka:${KAFKA_INT_PORT} --create --if-not-exists --topic telemetry.spans.parsed --replication-factor 1 --partitions 1
 
-
 kafka-topics --bootstrap-server kafka:${KAFKA_INT_PORT} --create --if-not-exists --topic tokens.events --replication-factor 1 --partitions 20
-
-kafka-topics --bootstrap-server kafka:${KAFKA_INT_PORT} --create --if-not-exists --topic tokens.events.table --replication-factor 1 --partitions 20
 
 echo -e 'Successfully created the following topics:'
 kafka-topics --bootstrap-server kafka:${KAFKA_INT_PORT} --list
