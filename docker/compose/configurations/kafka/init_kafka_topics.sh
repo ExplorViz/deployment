8db554d359f51d09f06bfad2fc5880400e748d24
Moritz Bugla <stu237520@mail.uni-kiel.de>
2026-07-25 15:09:14 +0200

    Rename telemetry entities topic to include other telemetry signals, modify ClickHouse schema materialized columns

--- docker/compose/configurations/kafka/init_kafka_topics.sh
+++ docker/compose/configurations/kafka/init_kafka_topics.sh
@@ -2,9 +2,7 @@
 
 echo -e 'Creating kafka topics'
 
-kafka-topics --bootstrap-server kafka:${KAFKA_INT_PORT} --create --if-not-exists --topic telemetry.spans.raw --replication-factor 1 --partitions 20
-
-kafka-topics --bootstrap-server kafka:${KAFKA_INT_PORT} --create --if-not-exists --topic telemetry.spans.parsed --replication-factor 1 --partitions 1
+kafka-topics --bootstrap-server kafka:${KAFKA_INT_PORT} --create --if-not-exists --topic telemetry.entities --replication-factor 1 --partitions 1
 
 kafka-topics --bootstrap-server kafka:${KAFKA_INT_PORT} --create --if-not-exists --topic tokens.events --replication-factor 1 --partitions 20
 
